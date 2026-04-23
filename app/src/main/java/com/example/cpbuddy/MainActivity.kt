8d757e8243da49f2d6c52466cfd2de4db792020a
adityapaul26 <adityapaul2603@gmail.com>
2026-04-23 13:03:58 +0530

    current-date-add

--- app/src/main/java/com/example/cpbuddy/MainActivity.kt
+++ app/src/main/java/com/example/cpbuddy/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,12 +15,20 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set today's date
+        val tvTodayDate = findViewById<TextView>(R.id.tvTodayDate)
+        val sdf = SimpleDateFormat("dd MMM, yyyy", Locale.getDefault())
+        tvTodayDate.text = sdf.format(Date())
 
         val recyclerView = findViewById<RecyclerView>(R.id.rvContests)
         val progressBar = findViewById<ProgressBar>(R.id.progressBar)
